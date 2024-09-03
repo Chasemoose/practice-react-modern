14d0d141104719ee3eeb34bb167ed1fc64cf1b5b
Jaro <jaroswidnik@interia.pl>
2024-09-03 14:10:06 +0200

    Changes in ex 4

--- 04/SpeedTest.js
+++ 04/SpeedTest.js
@@ -1,17 +1,46 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import useRandomItem from './hook';
 
-const SpeedTest = () => {
+const SpeedTest = function SpeedTest() {
     const [word, regenerateWord] = useRandomItem(['devmentor.pl', 'abc', 'JavaScript']);
+    const [text, setText] = useState('')
+    const [time, setTime] = useState(0)
+    const [length, setLength] = useState(0)
+
+    const intervalId = useRef(null)
 
     useEffect(() => {
         regenerateWord();
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
+    
+    useEffect(() => {
+        if(text === word) {
+            regenerateWord()
+            setLength((curr) => curr + text.length)
+            setText('')
+        }
+        
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [text])
+
+    function startInterval() {
+        intervalId.current = setInterval(() => {
+            setTime((currentTime) => currentTime + 1)
+        }, 1000)
+    }
+
+    function stopInterval() {
+        clearInterval(intervalId.current)
+
+    }
 
     return (
         <div>
             <h1>{word}</h1>
-            <input />
+            <h2>{time}</h2>
+            <h3>Ilość znaków na sec.: {time > 0 ? (length / time).toFixed(1) : 0}</h3>
+            <input value={text} onBlur={stopInterval} onFocus={startInterval} onChange={e => setText(e.target.value)}/>
         </div>
     );
 };
