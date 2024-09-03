53d2d3f80dd1d70c7289cf558c546356dd9f3e76
Jaro <jaroswidnik@interia.pl>
2024-09-03 15:10:56 +0200

    Changes in ex 5 more to come

--- 05/ContactForm.js
+++ 05/ContactForm.js
@@ -1,10 +1,61 @@
 import React from 'react';
 
-import account from './account';
+// import account from './account';
 
-const ContactForm = () => {
-    console.log(account);
-    return <form>??</form>;
+const ContactForm = function ContactForm() {
+    const init = {
+        name: '',
+        email: '',
+        phone: '',
+        topic: '',
+        content: '',
+    }
+
+
+    function reducer(state, action) {
+        switch(action.type) {
+            case "change": {
+                return {...state, [action.key]: action.value}
+            }
+            default:
+                return state
+        }
+        return state
+    }
+
+
+    const [state, dispatch] = useReducer(reducer, init)
+
+    const fieldsList = [
+        {name: 'name', type: 'text'}
+        {name: 'email', type: 'email'}
+        {name: 'phone', type: 'text'}
+        {name: 'topic', type: 'text'}
+        {name: 'content', type: 'textarea'}
+    ]
+
+    function renderFieldsList() {
+        return fieldsList.map(item => {
+            let tag;
+            if(item.type === 'textarea') {
+                tag = <textarea />
+            } else {
+                tag = <input onChange={e => dispatch({type: 'change', key: item.name, value: e.target.value})} id={input.name} name={item.name} type={item.type} />
+            }
+            return ( <div>
+                    <label htmlFor={item.name}>{item.name}</label>{tag}
+                    </div>
+                )
+        })
+    }
+
+
+    return <form>
+            {renderFieldsList()}
+            <div>
+                <input type="submit"></input>
+            </div>
+        </form>;
 };
 
 export default ContactForm;
