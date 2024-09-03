a5cc8d50fc28e81a70f79aefa37e9788fd8ad7c7
Jaro <jaroswidnik@interia.pl>
2024-09-03 12:33:03 +0200

    Changes in ex 3

--- 03/components/App.js
+++ 03/components/App.js
@@ -1,15 +1,19 @@
 // ./src/components/App.js
-import React from 'react';
-import Box from './Box';
-import Div from './Div';
+import React from "react";
+import Box from "./Box";
+import Div from "./Div";
 
-const App = () => {
-    return (
-        <section>
-            <Box />
-            <Div />
-        </section>
-    );
+import { TextContext } from "../context";
+
+const App = function App() {
+  return (
+    <section>
+      <Box />
+      <TextContext.provider value="sibling">
+        <Div />
+      </TextContext.provider>
+    </section>
+  );
 };
 
 export default App;
