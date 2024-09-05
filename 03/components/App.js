07ae8126bf63c135c20e9d71dc04e137d5b9e5d5
Jarek Stelmach <j.stelmach@onet.eu>
2024-09-05 22:27:38 +0200

    Finished ex3

--- 03/components/App.js
+++ 03/components/App.js
@@ -6,14 +6,14 @@ import Div from "./Div";
 import { TextContext } from "../context";
 
 const App = function App() {
-  return (
-    <section>
-      <Box />
-      <TextContext.provider value="sibling">
-        <Div />
-      </TextContext.provider>
-    </section>
-  );
+    return (
+        <section>
+            <Box />
+            <TextContext.Provider value="sibling">
+                <Div />
+            </TextContext.Provider>
+        </section>
+    );
 };
 
 export default App;
