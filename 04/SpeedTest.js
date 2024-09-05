fff0df4ad05766e32c7993ac79c3d0f42f7da853
Jarek Stelmach <j.stelmach@onet.eu>
2024-09-05 22:32:42 +0200

    Finished ex4

--- 04/SpeedTest.js
+++ 04/SpeedTest.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import useRandomItem from './hook';
 
 const SpeedTest = function SpeedTest() {
